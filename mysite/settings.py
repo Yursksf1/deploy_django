c1a2110e9eaf723eca312482ee83940e7f7da93c
yurley <yurs.ksf1@gmail.com>
2022-09-26 21:03:40 -0500

    fix namely

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(qmvby02=y$$(yzr&z=lpqyi+z139bic##ttjf6p$5a5h2y9=$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
